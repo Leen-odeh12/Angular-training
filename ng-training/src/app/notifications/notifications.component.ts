1ac883514e066e3dd39f80519f1767392a7fb7b2
leen <leenodeh287@gmail.com>
2023-09-18 00:21:42 +0300

    use style and template property

---
+++ ng-training/src/app/notifications/notifications.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications',
+  template: '<div><p> This website uses cookies to provide better user experiancs</p> </div>',
+  styles: ["div{  background-color: rgb(232, 139, 105); text-align: center; padding:1px}"]
+})
+export class NotificationsComponent {
+
+}
