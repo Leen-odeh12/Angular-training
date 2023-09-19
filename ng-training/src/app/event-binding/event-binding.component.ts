7d3dee740e759469db25e29d09a481f4150314db
leen <leenodeh287@gmail.com>
2023-09-19 19:57:01 +0300

    event-binding

---
+++ ng-training/src/app/event-binding/event-binding.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding',
+  templateUrl: './event-binding.component.html',
+  styleUrls: ['./event-binding.component.css']
+})
+export class EventBindingComponent {
+
+  value : string =''
+  show(event :Event){
+
+    this.value=(<HTMLInputElement>event.target).value;
+  }
+}
